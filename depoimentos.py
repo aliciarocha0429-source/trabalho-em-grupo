30c5ba6460314cab5fdeb33bcc83099f00779e05
DiegodosAnjos-ti <150624976+wallissonwh@users.noreply.github.com>
2026-09-09 10:17:17 -0300

    criandooapp

---
+++ depoimentos.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+class Depoimentos: 
+    def __init__(self, depoimento: str, pessoa, caso, ):
+        self.__depoimento = depoimento # TRANSCREVER TODO DEPOIMENTO DA PESSOA
+        self.__caso = caso # CLASS CASO
+        self.__pessoa = pessoa # CLASS PESSOA
+        self.__linha_do_tempo = [] # SAIU DE TAL LUGAR - PARTIU PRA TAL LUGAR - CHEGOU EM TAL LUGAR
+        self.__conclusao = "" # COLOCA TIPO: POSSIVEL INCOSSISTENCIA
+        self.__horario = datetime.now()
+
+    def get_depoimento(self):
+        print("DEPOIMENTO: ", self.__depoimento)
+
+    def get_conclusao(self):
+        print(f"O CASO FOI MARCADO COMO: {self.__conclusao}.")
+
+    def set_conclusao(self):
+        c = int(input("Olá, defina a conclusão do caso de acordo a analise do depoimento da pessoa.\n1 - TODO O DEPOIMENTO BATE;\n2 - A MAIORIA DO DEPOIMENTO BATE;\n3 - POSSÍVEL INCONSISTÊNCIA;\n4 - INCONSISTÊNCIA."))
+        if c == 1:
+            self.__conclusao = "✅ TODO DEPOIMENTO BATE."
+        elif c == 2:
+            self.__conclusao = "✅ O DEPOIMENTO BATE, MAS EXISTE INCONSISTÊNCIA."
+        elif c == 3:
+            self.__conclusao = "⚠️ POSSÍVEL INCONSISTÊNCIA"
+        elif c == 4:
+            self.__conclusao = "⚠️ INCONSISTÊNCIA"
+
+    def get_linha_do_tempo(self):
+        print(self.__linha_do_tempo)
+
+    def set_linha_do_tempo(self):
+        atualizacao = input("Adicione outras coisas na linha do tempo [digite 'SAIR' para parar de adicionar]: ")
+        while atualizacao.upper() != "SAIR":
+            self.__linha_do_tempo.append(atualizacao)
+            print(f"{atualizacao} adicionada a linha do tempo")
+
+            atualizacao = input("Adicione outras coisas na linha do tempo [digite 'SAIR' para parar de adicionar]: ")
+
+d1 = Depoimentos("TAL TALTATLALTATLTALTLA", "1", "2")
+
+d1.get_linha_do_tempo()
+d1.set_linha_do_tempo()
+d1.get_linha_do_tempo()
+d1.get_informacoes_depoimentos()
+d1.set_conclusao()
+d1.get_informacoes_depoimentos()
+
